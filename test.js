d595da7467af399ed9a2a5b8f48d9143c8f01026
Titus Wormer <tituswormer@gmail.com>
2023-07-12 09:31:26 +0200

    Change to use `exports`

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
-import {commentMarker} from './index.js'
+import {commentMarker} from 'mdast-comment-marker'
 
 test('commentMaker', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('mdast-comment-marker')).sort(), [
       'commentMarker'
     ])
   })
